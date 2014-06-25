e29636872aeb4c2a1d9c8f2ebf0165e331053f51
Alan Potter <alan.potter@calnexsol.com>
2014-06-25 14:23:37 +0100

    First checkin

---
+++ PhotoUploader/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PhotoUploader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbBody = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnImg = new System.Windows.Forms.Button();
+            this.lblImage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title:";
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbTitle.Location = new System.Drawing.Point(65, 43);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(512, 20);
+            this.tbTitle.TabIndex = 1;
+            this.tbTitle.Text = "Programmatic test";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Body";
+            // 
+            // tbBody
+            // 
+            this.tbBody.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbBody.Location = new System.Drawing.Point(65, 102);
+            this.tbBody.Multiline = true;
+            this.tbBody.Name = "tbBody";
+            this.tbBody.Size = new System.Drawing.Size(512, 302);
+            this.tbBody.TabIndex = 3;
+            this.tbBody.Text = "This is a message being generated from a file";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(501, 425);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnImg
+            // 
+            this.btnImg.Location = new System.Drawing.Point(16, 425);
+            this.btnImg.Name = "btnImg";
+            this.btnImg.Size = new System.Drawing.Size(75, 23);
+            this.btnImg.TabIndex = 5;
+            this.btnImg.Text = "Post Image";
+            this.btnImg.UseVisualStyleBackColor = true;
+            this.btnImg.Click += new System.EventHandler(this.btnImg_Click);
+            // 
+            // lblImage
+            // 
+            this.lblImage.AutoSize = true;
+            this.lblImage.Location = new System.Drawing.Point(97, 430);
+            this.lblImage.Name = "lblImage";
+            this.lblImage.Size = new System.Drawing.Size(84, 13);
+            this.lblImage.TabIndex = 6;
+            this.lblImage.Text = "C:\\testimage.jpg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 465);
+            this.Controls.Add(this.lblImage);
+            this.Controls.Add(this.btnImg);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tbBody);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbBody;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnImg;
+        private System.Windows.Forms.Label lblImage;
+
+    }
+}
+
---
+++ PhotoUploader/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
+using System.Text;
+using System.Windows.Forms;
+using CookComputing.XmlRpc;
+using Wordpress;
+using Wordpress.Xml;
+using Wordpress.Xml.Rpc;
+
+using xmlrpc;
+
+namespace PhotoUploader
+{
+    public struct blogInfo
+    {
+        public string title;
+        public string description;
+        public DateTime dateCreated;
+    }
+
+    public struct newMediaDescriptor
+    {
+        public string name;
+        public string type;
+        public string bits;
+        public bool overwrite;
+    };
+
+    public struct newMediaResponse
+    {
+        public string id;
+        public string file;
+        public string url;
+        public string type;
+    };
+
+    public interface IgetCatList
+    {
+        [CookComputing.XmlRpc.XmlRpcMethod("metaWeblog.newPost")]
+        string NewPost(int blogId, string strUserName,
+            string strPassword, blogInfo content, int publish);
+
+        [CookComputing.XmlRpc.XmlRpcMethod("metaWeblog.newMediaObject")]
+        newMediaResponse NewMedia(int blogId,
+            string username,
+            string password,
+            newMediaDescriptor data);
+    }
+
+
+    public partial class Form1 : Form
+    {
+
+        public XmlRpcClientProtocol clientProtocol;
+        public IgetCatList categories;
+
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // Username:    test
+            // Password:    DjF3iUfMDto0
+            blogInfo newBlogPost = default(blogInfo);
+            newBlogPost.title = tbTitle.Text;
+            newBlogPost.description = tbBody.Text;
+            newBlogPost.dateCreated = new DateTime(2009,10,11);
+
+            categories = (IgetCatList)XmlRpcProxyGen.Create(typeof(IgetCatList));
+            clientProtocol = (XmlRpcClientProtocol)categories;
+            clientProtocol.Url = "http://www.thepotters.org/test/wpl/xmlrpc.php";
+            string result = null;
+            result = "";
+            try
+            {
+                result = categories.NewPost(1, "test", "DjF3iUfMDto0", newBlogPost, 1); 
+                MessageBox.Show("Posted to Blog successfullly! Post ID : " + result); 
+                tbBody.Text = "";
+                tbTitle.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnImg_Click(object sender, EventArgs e)
+        {
+            var newMediaPost = default(newMediaDescriptor);
+            newMediaPost.name = "ProgramaticallyUploadedImage.jpg";
+            newMediaPost.type = "image/jpeg";
+            newMediaPost.overwrite = false;
+            
+            // Now we read in the picture and turn it into a byte array
+            byte[] fileAsBytes;
+
+            using (var fs = new FileStream(lblImage.Text, FileMode.Open))
+            {
+                fileAsBytes = new byte[fs.Length];
+                fs.Read(fileAsBytes, 0, (int)fs.Length);
+            }
+
+            // And convert it to base-64 encoding
+            newMediaPost.bits = Convert.ToBase64String(fileAsBytes);
+
+
+
+            categories = (IgetCatList)XmlRpcProxyGen.Create(typeof(IgetCatList));
+            clientProtocol = (XmlRpcClientProtocol)categories;
+            clientProtocol.Url = "http://www.thepotters.org/test/wpl/xmlrpc.php";
+
+            try
+            {
+                var result = categories.NewMedia(
+                    3, "test", "DjF3iUfMDto0", newMediaPost);
+
+
+                MessageBox.Show(string.Format(
+                    "ID: {0}\r\nFile: {1}\r\nURL: {2}\r\nType:{3}",
+                    result.id,
+                    result.file,
+                    result.url,
+                    result.type));
+
+                tbBody.Text = "";
+                tbTitle.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        
+        }
+    }
+}
